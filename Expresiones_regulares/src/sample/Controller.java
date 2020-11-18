806d58d0f9196d65c19df1963d2b253d27f81f56
Diego-Vazquez <diegoavaz2000@gmail.com>
2020-11-18 16:15:30 -0700

    Expresiones regulares

--- Expresiones_regulares/src/sample/Controller.java
+++ Expresiones_regulares/src/sample/Controller.java
@@ -16,7 +16,22 @@ public class Controller {
     @FXML
     Label resultado;
     @FXML protected void initialize(){
-        casos.getItems().addAll("ejercicio 1","Ejercicio 2","validar password");
+        casos.getItems().addAll(
+                "ejercicio 1",
+                "Ejercicio 2",
+                "validar password",
+                "validar que no comienza con db",
+                "validar que termina con count",
+                "validar dos nombres",
+                "validar mac address",
+                "validar palabra que comience\n y termine con mayuscula",
+                "validar palabra que comience con mayuscula\n y termine con minuscula",
+                "validar numero binario de 8 digitos",
+                "validar numero hexadecimal",
+                "validar operacion de un digito",
+                "validar numero de control ITSNCG",
+                "validar curp"
+                );
     }
 
     public void procesar(){
@@ -36,8 +51,20 @@ public class Controller {
                 //y de una longitud de  8-16 digitos
                 expresion="((^[A-Z]{2,4})([a-z]{2,4})([0-9]{2,4})([*|#]{2,4}$))";
                 System.out.println("aqui");
+                break;
 
-            }
+            } case 3:{expresion="(^[^db])([a-zA-Z0-9]+)";break;}
+            case 4:{expresion="([\\w]+)([count]$)";break;}
+            case 5:{expresion="[a-zA-Z\\s]+";break;}
+            case 6:{expresion="^([0-9a-f]{1,2}[:]){5}([0-9a-f]{1,2})$";break;}
+            case 7:{expresion="(^[A-Z]+)([a-zA-Z]+)([A-Z]+$)";break;}
+            case 8:{expresion="(^[A-Z]+)([a-zA-Z]+)([a-z]+$)";break;}
+            case 9:{expresion="^[0-1]+$";break;}
+            case 10:{expresion="^[0-9a-fA-F]+$";break;}
+            case 11:{expresion="(^[0-9]{1,1})([+|-|*|/]+)([0-9]{1,1}$)";break;}
+            case 12:{expresion="(^[0-9]{2,2})([cg]{2,2})([0-9]{4,4}$)";break;}
+            case 13:{expresion="^([A-Z][AEIOUX][A-Z]{2}\\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\\d])(\\d)$";break;}
+
         }
         String texto=entrada.getText();
         Pattern patron=Pattern.compile(expresion);
